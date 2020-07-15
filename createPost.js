ea4b950b9aad083c84c0cc9ffbc30eaa9389ecd4
shrey-shriyan <shreyshriyan.com>
2020-07-15 22:27:55 +0530

    created post creation logic

--- createPost.js
+++ createPost.js
@@ -1,8 +1,8 @@
-
-let posts = []
+let form = document.querySelector('form')
+let body = document.querySelector('body')
 
 class Post{
-    constructor(id,title,category,content,author){
+    constructor(id,title,content,category,author){
         this.id = id
         this.title = title
         this.content = content
@@ -20,15 +20,29 @@ class Post{
         }
     }
 
-    appendPost = () =>{
-        posts.push(create_post_object)
-    }
 }
 
-let form = document.getElementBy
+submitPost = () => {
+    event.preventDefault()
+    let language = document.getElementById('postCategory')
+    let postTitle = document.getElementById('postTitle')
+    let postContent = document.getElementById('postContent')
+
+    let postObjectParsed = JSON.parse(localStorage.getItem('postObject'))
+    let id = postObjectParsed[`${language.value}`].length + 1
+    
+    let temp = new Post(id,postTitle.value,postContent.value,language.value,'admin')
+    postObjectParsed[`${language.value}`].push(temp.createPostObject())
+    localStorage.setItem('postObject',JSON.stringify(postObjectParsed))
+
+    alert('✓ post submitted')
+    
+    postTitle.value = ""
+    postContent.value = ""
+}
+
+
 
 form.addEventListener('submit',submitPost)
-
-let temp = new Post(id,title,content,author,category)
 
 
